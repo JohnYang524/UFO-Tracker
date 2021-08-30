e77fc6e6b09cd5ce27b5038247054ce1116599df
BPI\jyang <john.yang@zovio.com>
2021-08-30 12:57:06 -0700

    Add test data only for current displaying tab.

--- UFOTracker/app/src/main/java/com/lyft/android/ufotracker/MainActivity.java
+++ UFOTracker/app/src/main/java/com/lyft/android/ufotracker/MainActivity.java
@@ -15,10 +15,14 @@ import com.lyft.android.ufotracker.ui.model.Sighting;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * MainActivity that holds a AppBarLayout and a TabLayout that is set up using a ViewPager.
+ */
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private List<ListRefreshListener> listRefreshListeners;
+    private int currentTab = 0; // For testing purpose, we need to save current displaying tab index for inserting corresponding test data.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +39,26 @@ public class MainActivity extends AppCompatActivity {
 
         binding.ivAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { // Add hardcoded Sighting objects for testing purpose
-                // Notify all listeners
-                for (ListRefreshListener listener : listRefreshListeners) {
+            public void onClick(View v) { // Add hardcoded Sighting objects for testing
+                for (ListRefreshListener listener : listRefreshListeners) {// Notify all listeners
                     if (listener != null) {
-                        listener.onNewSightingAdded(new Sighting("March 25, 2021 @ 9:00 PM", Sighting.SightingType.LAMPSHADE, "19 knots"));
-                        listener.onNewSightingAdded(new Sighting("March 22, 2021 @ 11:00 PM", Sighting.SightingType.MYSTERIOUS_LIGHTS, "5 knots"));
+                        listener.onNewSightingAdded(currentTab == 0 ?
+                                new Sighting("March 25, 2021 @ 9:00 PM", Sighting.SightingType.LAMPSHADE, "19 knots") :
+                                new Sighting("March 22, 2021 @ 11:00 PM", Sighting.SightingType.MYSTERIOUS_LIGHTS, "5 knots"));
                     }
                 }
             }
+        });
+        binding.tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // Save current selected tab
+                currentTab = tab.getPosition();
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
         });
 
         listRefreshListeners = new ArrayList<>();
