21aec45be4eb7a971f43ea0aad4bbe90662d77b9
BPI\jyang <john.yang@zovio.com>
2021-10-05 23:27:08 -0700

    Updated UI for second fragment. (Not in flow yet)

--- UFOTracker/app/src/main/java/com/demo/android/ufotracker/ui/view/DetailsFragment.java
+++ UFOTracker/app/src/main/java/com/demo/android/ufotracker/ui/view/DetailsFragment.java
@@ -8,8 +8,10 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
-import com.demo.android.ufotracker.databinding.FragmentDetailBinding;
+import com.demo.android.ufotracker.R;
+import com.demo.android.ufotracker.databinding.FragmentSecondBinding;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -18,13 +20,22 @@ public class DetailsFragment extends Fragment {
     private static final boolean mIsDebuggable = true;
     private static final String  TAG           = DetailsFragment.class.getName();
 
-    private FragmentDetailBinding binding;
+    private FragmentSecondBinding binding;
 
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment.findNavController(DetailsFragment.this)
+                        .navigate(R.id.action_2nd_to_list);
+            }
+        });
+        return root;
     }
 
     @Override
